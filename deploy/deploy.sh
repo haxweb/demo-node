e52f3ee1654710b03154f1b26dc762cc163add44
hacksel <axelagarrat@gmail.com>
2016-08-31 01:06:28 +0200

    Update with rolling-update via apply, and deployment object for rc

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,7 +12,6 @@ if [ "$appreplication" == "" ]; then
         echo "Application ${APP_NAME} not yet deployed. deploying..."
 	${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl create -f deploy/replica.yml -f deploy/service.yml
 else
-        echo "Application replication controller found, patching ${APP_NAME} with new image..."
-	${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl patch rc ${APP_NAME} \
-		-p '{"spec":{"template":{"spec":{"containers":[{"name":"'${APP_NAME}'","image":"gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:'"$CIRCLE_SHA1"'"}]}}}}'
+        echo "Application replication controller found, rolling-update via apply command on ${APP_NAME} for new image..."
+	${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl apply -f deploy/replica.yml
 fi
