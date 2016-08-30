9fd5fef7fba75a8271044c99d389baa84eab70db
root <root@ns3003016.ip-151-80-42.eu>
2016-08-30 19:50:41 +0200

    testing with sed for .bashrc

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@
 set -e
 
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch rc ${APP_NAME} -p '{"spec":{"template":{"spec":{"containers":[{"name":"'${APP_NAME}'","image":"gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:'"$CIRCLE_SHA1"'"}]}}}}'
+
+
+/home/ubuntu/demo-node/google-cloud-sdk/bin/kubectl patch rc ${APP_NAME} \ 
+	-p '{"spec":{"template":{"spec":{"containers":[{"name":"'${APP_NAME}'","image":"gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:'"$CIRCLE_SHA1"'"}]}}}}'
+
