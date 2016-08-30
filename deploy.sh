215a8c0771446a03fea0507e2a71bacb3b1fa752
hacksel <axelagarrat@gmail.com>
2016-08-30 21:46:29 +0200

    Updating circle.yml

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 # Exit on any error
 set -e
 
+docker build -t gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1 .
 
+docker tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1 gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:latest
 
+${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1
 
-/home/ubuntu/demo-node/google-cloud-sdk/bin/kubectl patch rc ${APP_NAME} \ 
+${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl patch rc ${APP_NAME} \
 	-p '{"spec":{"template":{"spec":{"containers":[{"name":"'${APP_NAME}'","image":"gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:'"$CIRCLE_SHA1"'"}]}}}}'
-
