30f6ddf1093952897fb46a5bcec77577af3e4c31
hacksel <axelagarrat@gmail.com>
2016-08-30 22:31:38 +0200

    Updating deploy.sh with config cluster files yml

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,5 +9,15 @@ docker tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1 gcr.io/${PROJECT_
 
 ${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1
 
-${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl patch rc ${APP_NAME} \
-	-p '{"spec":{"template":{"spec":{"containers":[{"name":"'${APP_NAME}'","image":"gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:'"$CIRCLE_SHA1"'"}]}}}}'
+
+appreplication=$(`${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl get rc | grep $APP_NAME`)
+
+if [ "$appreplication" == "" ]; then
+        echo "Application ${APP_NAME} not yet deployed. deploying..."
+	${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl create -f replica.yml -f service.yml
+else
+        echo "Application replication controller found, patching ${APP_NAME} with new image..."
+	${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/kubectl patch rc ${APP_NAME} \
+		-p '{"spec":{"template":{"spec":{"containers":[{"name":"'${APP_NAME}'","image":"gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:'"$CIRCLE_SHA1"'"}]}}}}'
+fi
+
