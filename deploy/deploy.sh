074338a979c1195ceeac0786f7d35abce0d15b84
hacksel <axelagarrat@gmail.com>
2016-08-30 23:59:06 +0200

    Update deploy.sh with latest tag

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ docker build -t gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1 .
 
 docker tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1 gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:latest
 
-${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:$CIRCLE_SHA1
+${CLOUDSDK_INSTALL_DIR}/google-cloud-sdk/bin/gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE}:latest
 
 appreplication=`$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin/kubectl get rc | grep "$APP_NAME"`
 
