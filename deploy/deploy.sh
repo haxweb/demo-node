decfd0273c6602309f8548300fd688492abdf193
hacksel <axelagarrat@gmail.com>
2016-09-07 22:03:47 +0200

    Auto setting app name on deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,8 +11,16 @@ appreplication=`$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin/kubectl get deploymen
 
 echo "Update image tag with circle sha1 version..."
 sed -i -e "s/__IMAGE__/gcr.io\/$PROJECT_NAME\/$DOCKER_IMAGE:$CIRCLE_SHA1/" deploy/replica.yml
-cat deploy/replica.yml
 echo "Done."
+
+echo "Update app name on deployment descriptor..."
+sed -i -e "s/__APP_NAME__/$APP_NAME/g" deploy/replica.yml
+sed -i -e "s/__APP_NAME__/$APP_NAME/g" deploy/service.yml
+echo deploy/replica.yml
+echo "Done."
+
+
+APP_NAME
 
 if [ "$appreplication" == "" ]; then
         echo "Application ${APP_NAME} not yet deployed. deploying..."
